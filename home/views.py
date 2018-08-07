313050d72f3d22a186b421bd1817cd0ffb6e4b2f
Kazi-Md-Abdullah-Al-Mubin <kazi.mubin1234@gmail.com>
2018-08-07 17:22:52 +0600

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -1,27 +1,49 @@
 from django.shortcuts import render
 import datetime
-from . import models
+from .models import User
+import random
+
+
+def output():
+    sitetype = User.site_type
+
+    # probabilistic algorithm here.
+
+    # total number of visits
+    a = User.objects.all().count()
+    print(a)
+
+    numberofostypes = User.objects.filter(name)
+
+    # total_user = len(a.Objects.all())
+    # print(inp.id)
+    # print(inp.browser)
+    # print(inp.os)
+    # print(inp.device)
+    # print(inp.site_type)
+    # print(inp.present)
+
+    return sitetype
 
 
 # Create your views here.
+
 def index(request):
+    type = ["Electronics", "Fashion", "Furniture", "Chemical", "Ride"]
+
     # tem = loader.get_template('home/index.html')
-    browser = request.user_agent.browser.family
-    os = request.user_agent.os.family
-    device = request.user_agent.device.family
-    present = datetime.datetime.now().strftime('%H:%M:%S')
+    inp = User()
+    inp.os = request.user_agent.os.family
+    inp.device = request.user_agent.device.family
+    inp.save()
 
-    info = {'browser': browser,
-            'os': os,
-            'device': device,
-            'time': present,
-            }
+    # inp.id = random.randrange(0, 100000000)
+    # inp.browser = request.user_agent.browser.family
+    # inp.present = datetime.datetime.now().strftime('%H:%M:%S')
+    # inp.site_type = random.choice(type)
+
+    out = output()
+
+    info = {'output': out, }
+
     return render(request, 'home/index.html', info)
-
-def output(request):
-    a = models.User()
-
-    total_user = len(a.Objects.all())
-
-
-
